b5e033857862e941af22e59c3dccfc1d850af8bc
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-28 23:34:26 +0530

    load html by using DOM parsing

--- 1.php
+++ 1.php
@@ -4,16 +4,9 @@
  $pokemon_doc= new DOMDocument();
 libxml_use_internal_errors(TRUE);
 if(!empty($html)){
-    $pokemon_doc->loadHTML($html);
-    libxml_clear_errors();
-    $pokemon_xpath=new DOMXPath($pokemon_doc);
+    echo $html;
     
-    $pokemon_row=$pokemon_xpath->query('//h2[@id]');
-    
-    if($pokemon_row->length>0){
-        foreach($pokemon_row as $row){
-            echo $row->nodeValue."<br/>";
         }
-    }
-}
+    
+
 ?>
