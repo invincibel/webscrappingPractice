6cb39a76cb52c6593bb33b7c838dac054002b840
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-28 23:01:37 +0530

    updated document scrapping

--- 1.php
+++ 1.php
@@ -1,5 +1,6 @@
 <?php
- $html=file_get_contents('http://pokemondb.net/evolution');
+ $html = file_get_contents('http://pokemondb.net/evolution'); 
+
  $pokemon_doc= new DOMDocument();
 libxml_use_internal_errors(TRUE);
 if(!empty($html)){
