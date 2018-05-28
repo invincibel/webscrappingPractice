d0e79d0cf31457fd08962b43e424fd3e96b92a3a
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-29 00:23:27 +0530

    Updated DOM parsing

--- 1.php
+++ 1.php
@@ -4,8 +4,19 @@
  $pokemon_doc= new DOMDocument();
 libxml_use_internal_errors(TRUE);//disable libxml errors
 if(!empty($html)){//check whether the html is returned or not
-    echo $html;
-    
+              $pokemon_doc->loadHTML($html);
+	libxml_clear_errors(); //remove errors for yucky html
+	
+	$pokemon_xpath = new DOMXPath($pokemon_doc);
+
+	//get all the h2's with an id
+	$pokemon_row = $pokemon_xpath->query('//h2[@id]');
+
+	if($pokemon_row->length > 0){
+		foreach($pokemon_row as $row){
+			echo $row->nodeValue . "<br/>";
+		}
+	}    
         }
     
 
