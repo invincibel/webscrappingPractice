c61419c435e4ab277723581bf70cd9a962258707
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-28 23:38:46 +0530

    updated DOM parsing

--- 1.php
+++ 1.php
@@ -1,9 +1,9 @@
 <?php
- $html = file_get_contents('http://pokemondb.net/evolution'); 
+ $html = file_get_contents('http://pokemondb.net/evolution');// for gettingthe html 
 
  $pokemon_doc= new DOMDocument();
-libxml_use_internal_errors(TRUE);
-if(!empty($html)){
+libxml_use_internal_errors(TRUE);//disable libxml errors
+if(!empty($html)){//check whether tghe html is returned or not
     echo $html;
     
         }
