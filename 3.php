9cf5aba9f0d0fab3d37150e0dc9ff46b9b08d6a2
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-30 18:34:55 +0530

    updated

--- 3.php
+++ 3.php
@@ -7,6 +7,19 @@ curl_setopt($ch,CURLOPT_URL,"https://namanvashistha.github.io/");
 curl_exec($ch);
 //closing curl
 curl_close($ch);
-
-
 ?>
+<html>
+    <body>
+        >Hello World!</p>
+
+<p>The DOM is very useful.</p>
+<p>This example demonstrates the <b>getElementsByTagName</b> method</p>
+
+<p id="demo"></p>
+        <script>
+var x = document.getElementsByTagName("p");
+document.getElementById("demo").innerHTML = 
+'The first paragraph (index 0) is: ' + x[0].innerHTML;
+        </script>
+    </body>
+</html>
