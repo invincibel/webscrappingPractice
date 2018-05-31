0a3a47dfd3b8fcf87d6942c8f6e9d54dfffa4d53
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-31 13:38:08 +0530

    Updated

--- project_1/index.php
+++ project_1/index.php
@@ -1,19 +1,23 @@
 <?php
     function fetchPage($name)
     {
-        
+        @$html = file_get_contents('https://github.com/'.$name);
+        $newD= new DOMDocument();
+        libxml_use_internal_errors(TRUE);
+        if(!empty($html)){
+           $newD->loadHTML($html);
+            libxml_clear_errors();
+            $pokemon_xpath = new DOMXPath($newD);
+            $row = $pokemon_xpath->query('//li');
+            foreach($row as $r)
+                echo $r->nodeValue;
         }
     }
  if(isset($_POST['Uname'])){$name=$_POST['Uname'];}
 ?>
-<form action="http://localhost:8080/scapper/webscrappingPractice/project_1/index.php" method="post">
-    <input type="text" name="Uname" required/><input type="submit" value="submit">
-</form>
+
 <?php
 if(isset($name)){ fetchPage($name);}?>
-<body>
-    <script>
-        var box = document.getElementsByTagName("nav");
-        box[0].style.visibility="hidden";
-    </script>
-</body>
+    <form action="http://localhost:8080/scapper/webscrappingPractice/project_1/index.php" method="post">
+    <input type="text" name="Uname" required/><input type="submit" value="submit">
+</form>
