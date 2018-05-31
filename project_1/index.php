6e5614adbd6b905b3f872ae40b1da8850de178d6
invincibel <31722976+invincibel@users.noreply.github.com>
2018-05-31 14:24:51 +0530

    Updated

--- project_1/index.php
+++ project_1/index.php
@@ -1,16 +1,27 @@
+<form action="http://localhost:8080/scapper/webscrappingPractice/project_1/index.php" method="post">
+    <input type="text" name="Uname" required/><input type="submit" value="submit">
+</form>
 <?php
     function fetchPage($name)
     {
-        @$html = file_get_contents('https://github.com/'.$name);
+        @$html = file_get_contents('https://github.com/'.$name."?tab=repositories");
+        @$html2 = file_get_contents('https://github.com/'.$name."?tab=stars");
         $newD= new DOMDocument();
+               $newD2= new DOMDocument();
         libxml_use_internal_errors(TRUE);
-        if(!empty($html)){
-           $newD->loadHTML($html);
+        if(!empty($html) && !empty($html2)){
+           $newD->loadHTML($html);$newD2->loadHTML($html2);
             libxml_clear_errors();
             $pokemon_xpath = new DOMXPath($newD);
-            $row = $pokemon_xpath->query('//li');
-            foreach($row as $r)
-                echo $r->nodeValue;
+            $pokemon_xpath2 = new DOMXPath($newD2);
+            $row = $pokemon_xpath->query('//h3');
+            $row2 = $pokemon_xpath2->query('//h3');
+            echo "<h1>Your Repositries</h1><br>";
+            foreach($row as $r){
+                echo $r->nodeValue."<br>";}
+            echo "<h1>Your stars repositries</h1><br>";
+            foreach($row2 as $r){
+                echo $r->nodeValue."<br>";}
         }
     }
  if(isset($_POST['Uname'])){$name=$_POST['Uname'];}
@@ -18,6 +29,4 @@
 
 <?php
 if(isset($name)){ fetchPage($name);}?>
-    <form action="http://localhost:8080/scapper/webscrappingPractice/project_1/index.php" method="post">
-    <input type="text" name="Uname" required/><input type="submit" value="submit">
-</form>
+    
